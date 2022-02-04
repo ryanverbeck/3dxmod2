bc11ce5943f38a74576c7d3f5b7e7dc40fc43d13
Ryan Verbeck <ryansurfer98@gmail.com>
2022-02-03 19:06:02 -0800

    dxgi->Present is now hooked.

--- WindowsPlayer/WindowsPlayer/Main.cpp
+++ WindowsPlayer/WindowsPlayer/Main.cpp
@@ -19,11 +19,15 @@ laucher comes in all Unity distributions. 3DxChat as of this writing is on Unity
 This launcher sets up core hooks before calling UnityMain. This launcher hooks numerous things(see below) but one core hook is GetModuleFileNameW.
 Hooking GetModuleFileNameW allows a cleaner user experience during the installation process and gets around the engine thinking the game needs to be
 patched if the user just dropped in a different launcher executable. So we tell the user just to input the installation folder and return that value
-fromm the hooked GetModuleFileNameW function.
+fromm the hooked GetModuleFileNameW function and the game is none the wiser.
 
 A dll in Windows only gets loaded into a process once.
 The launcher takes advantage of that and 
     - Loads d3d11.dll before the engine wants to load it and hooks D3D11CreateDevice.
+    - Loads dxgi.dll before the engine wants to load it and hooks CreateDXGIFactory1.
+              UnityPlayer.dll at 0x0000000180554F20 is were the engine actually calls CreateDXGIFactory1.
+    - Hook CreateSwapChainForHwnd so we can hook the present call.
+    - 
 
 
 When D3D11CreateDevice gets called by the engine, it now calls our function instead and we grab some variables and hook CreateTexture2D 
@@ -49,8 +53,10 @@ Now that we have CreateTexture2D hooked we now have the ability to monitor textu
 
 #include "MinHook.h"
 #include "d3d11_vmt.h"
+#include <dxgi1_2.h>
 
 ID3D11Device* unityD3D11Device = nullptr;
+IDXGIFactory* unityDXGIFactory = nullptr;
 ID3D11DeviceContext* unityD3D11ImmediateContext = nullptr;
 
 // Hint that the discrete gpu should be enabled on optimus/enduro systems
@@ -62,12 +68,61 @@ extern "C"
     __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 };
 
+HRESULT(*PresentActual)(IDXGIFactory* dxgiFactory, UINT SyncInterval, UINT Flags);
+HRESULT STDMETHODCALLTYPE PresentNew(IDXGIFactory* dxgiFactory, UINT SyncInterval, UINT Flags) {
+    return PresentActual(dxgiFactory, SyncInterval, Flags);
+}
+
+HRESULT CreateSwapChainForHwnd;
+
+HRESULT(*CreateSwapChainForHwndActual)(IDXGIFactory* factory, IUnknown* pDevice, HWND hWnd, const DXGI_SWAP_CHAIN_DESC1* pDesc, const DXGI_SWAP_CHAIN_FULLSCREEN_DESC* pFullscreenDesc, IDXGIOutput* pRestrictToOutput, IDXGISwapChain1** ppSwapChain);
+HRESULT STDMETHODCALLTYPE CreateSwapChainForHwndNew(IDXGIFactory *factory, IUnknown* pDevice, HWND hWnd, const DXGI_SWAP_CHAIN_DESC1* pDesc, const DXGI_SWAP_CHAIN_FULLSCREEN_DESC* pFullscreenDesc, IDXGIOutput* pRestrictToOutput, IDXGISwapChain1** ppSwapChain) {
+    HRESULT hr = CreateSwapChainForHwndActual(factory, pDevice, hWnd, pDesc, pFullscreenDesc, pRestrictToOutput, ppSwapChain);
+
+    // Hook the present function.
+    static bool setHook = false;
+    if (!setHook)
+    {
+        intptr_t** vtable = *reinterpret_cast<intptr_t***>(*ppSwapChain);// ->CreateTexture2D;
+        intptr_t* function = vtable[(int)IDXGISwapChainVMT::Present];
+        MH_CreateHook(function, PresentNew, (LPVOID*)&PresentActual);
+        MH_EnableHook(function);
+    
+        setHook = true;
+    }
+
+    return hr;
+}
+
 // Tell Unity we are in the right folder with the right executable name.
 DWORD (*GetModuleFileNameWActual)(HMODULE hModule, LPWSTR  lpFilename, DWORD   nSize);
 DWORD GetModuleFileNameWNew(HMODULE hModule, LPWSTR  lpFilename, DWORD   nSize) {
     // TODO: Move to external file don't hard code this shit!
     wcscpy(lpFilename, L"D:\\Games\\3DXChat2.8\\3DXChat.exe");
     return 1;
+}
+
+HRESULT (*CreateDXGIFactory1Actual)(REFIID riid, void** ppFactory);
+HRESULT CreateDXGIFactoryNew(REFIID riid, void** ppFactory) {
+    HRESULT hr = CreateDXGIFactory1Actual(riid, ppFactory);
+
+    unityDXGIFactory = (IDXGIFactory*)*ppFactory;
+
+    // Hook the CreateSwapChain function.
+    static bool setHook = false;
+    if (!setHook)
+    {
+        intptr_t** vtable = *reinterpret_cast<intptr_t***>(unityDXGIFactory);// ->CreateTexture2D;
+        intptr_t* function = vtable[15];
+        MH_CreateHook(function, CreateSwapChainForHwndNew, (LPVOID*)&CreateSwapChainForHwndActual);
+        MH_EnableHook(function);
+
+        setHook = true;
+    }
+
+    
+
+    return hr;
 }
 
 HRESULT  (*D3D11CreateTexture2DActual)(ID3D11Device* unityD3D11Device, const D3D11_TEXTURE2D_DESC* pDesc, const D3D11_SUBRESOURCE_DATA* pInitialData, ID3D11Texture2D** ppTexture2D);
@@ -115,6 +170,15 @@ void DetourD3D11CreateDevice(void)
     MH_EnableHook(function);
 }
 
+void DetourDXGICreateFactory(void)
+{
+    HMODULE dxgidll = LoadLibraryA("dxgi.dll");
+    void* function = (LPVOID)GetProcAddress(dxgidll, "CreateDXGIFactory1");
+    MH_CreateHook(function, CreateDXGIFactoryNew, (LPVOID*)&CreateDXGIFactory1Actual);
+    MH_EnableHook(function);
+}
+
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
 {
     MH_Initialize();
@@ -126,6 +190,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     }
 
     DetourD3D11CreateDevice();
+
+    DetourDXGICreateFactory();
 
     // Make the call into UnityMain will launch 3dx chat.
     return UnityMain(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
